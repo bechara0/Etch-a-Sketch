b36277e8b7ddb4a7b4f1e32327ae7e73bade811a
bechara0 <jjbechara@gmail.come>
2023-08-31 15:44:29 -0300

    por terminar

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const container = document.querySelector("#container");
 container.setAttribute(
   "style",
-  "display: flex; flex-wrap: wrap; width: 80vh; height:100vh"
+  "display: flex; flex-wrap: wrap; width: 600px; height:70vh; marging: 0 auto; padding: 0"
 );
 
-for (i = 0; i <= 16 * 16; i++) {
+for (i = 1; i <= 16 * 16; i++) {
   const divCreator = document.createElement("div");
   divCreator.setAttribute(
     "style",
-    "border: 1px solid black; width: calc(100% / 16); height: calc(100% / 16)"
+    "border: 2px solid black; width: calc(100% / 18); height: calc(100% / 17); marging: 0; padding: 0"
   );
+  divCreator.addEventListener("mouseenter", () => {
+    divCreator.style.background = "red";
+  });
   container.appendChild(divCreator);
 }
